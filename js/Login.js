07615654e7be744ef4c69ac827698197b6662a6e
Lucio <89551548+LucioF17@users.noreply.github.com>
2022-08-09 19:22:18 -0300

    Add files via upload

---
+++ js/Login.js
@@ -0,0 +1,14 @@
+function recogerDatos(){
+
+let dato1 = document.getElementById("user").value;
+let dato2 = document.getElementById("pass").value;
+let resultado = "Username: "+ dato1 +" "+ "Password: "+ dato2;
+console.log(resultado)
+   
+}
+
+
+
+
+
+
